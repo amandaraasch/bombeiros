ea7c187760c2544e75d5a7bfc38910445b60f2c5
andrebalexandre <andre_alexandre@estudante.sesisenai.org.br>
2023-10-12 14:29:53 -0300

    nn rolou

---
+++ html/form_cond.php
@@ -0,0 +1,25 @@
+<?php
+include("../conecta.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtenha os valores das caixas de seleção do POST
+
+    //bobo
+
+    $opcao1 = isset($_POST["opcao1"]) ? "Deitada" : "";
+    $opcao2 = isset($_POST["opcao2"]) ? "Semi-sentada" : "";
+    $opcao3 = isset($_POST["opcao3"]) ? "Sentada" : "";
+
+    $comando = $pdo->prepare("INSERT INTO form_cond (opcao1, opcao2, opcao3) VALUES (:op1, :op2, :op3)");
+
+    $comando->bindParam(":op1", $opcao1);
+    $comando->bindParam(":op2", $opcao2);
+    $comando->bindParam(":op3", $opcao3);
+
+    if ($comando->execute()) {
+        echo '{"Resp":1}';
+    } else {
+        echo '{"Resp":0}';
+    }
+}
+?>
