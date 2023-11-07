42ae4e8029a04972cd5e58f704a028934d3da74a
amandaraasch <amanda_raasch@estudante.sesisenai.org.br>
2023-11-07 11:28:51 -0300

    andre prt 3

--- html/lo_tra.php
+++ html/lo_tra.php
@@ -3,6 +3,9 @@ session_start();
 include("../conecta.php");
 
 $local= $_POST["resultado1"];
+$lado= $_POST["resultado2"];
+$face= $_POST["resultado3"];
+$tipo= $_POST["resultado4"];
 
 $comando = $pdo->prepare("INSERT INTO lo_tra (nOcorrencia, `local`, lado, face, tipo) VALUES (:nOco, :l, :lado, :face, :tipo)");
 
@@ -11,7 +14,8 @@ $comando = $pdo->prepare("INSERT INTO lo_tra (nOcorrencia, `local`, lado, face, 
     $comando->bindParam(":nOco", $ocorrencia);
     $comando->bindParam(":l", $local);
     $comando->bindParam(":lado", $lado);
-    $comando->bindParam(":op3", $opcao3);
+    $comando->bindParam(":face", $face);
+    $comando->bindParam(":tipo", $tipo);
 
 
 
